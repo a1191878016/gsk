0fe3a622ee26295547f3e12a13b4b10a8913c9d3
GU ms <1191878016@qq.com>
2019-02-10 13:06:05 +0800

    整理

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"http://192.168.149.23/api/api/v1"',
-  // BASE_API: '"http://192.168.63.77:8081/api/v1"',
-   BASE_API: '"https://www.easy-mock.com/mock/5bce7729eaf09a28dd3eb6b3/example"',
+  BASE_API: '"https://www.easy-mock.com/mock/5bfbb3837b03d66f9fa6c2e3/example_gsk"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.149.23/api/api/v1"',
-  //BASE_API: '"https://www.easy-mock.com/mock/5bce7729eaf09a28dd3eb6b3/example"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '"https://www.easy-mock.com/mock/5bfbb3837b03d66f9fa6c2e3/example_gsk"',
 }
